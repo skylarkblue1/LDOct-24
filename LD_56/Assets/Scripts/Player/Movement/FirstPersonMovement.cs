0b7ea1f27d3b4a11ad5fa4a8672e13d7bd331c54
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-10-04 19:16:08 -0700

    implment wasd movement

--- LD_56/Assets/Scripts/Player/Movement/FirstPersonMovement.cs
+++ LD_56/Assets/Scripts/Player/Movement/FirstPersonMovement.cs
@@ -1,39 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class FirstPersonMovement : MonoBehaviour
 {
-    // [SerializeField]
-    // float forwardSpeed;
-    // [SerializeField]
-    // float backSpeed;
+    [Header("Movement Speed")]
+    [SerializeField]
+    float forwardSpeed;
+    [SerializeField]
+    float backSpeed;
+    [SerializeField]
+    float strafeSpeed;
 
-    // [SerializeField]
-    // float strafeSpeed;
+    PlayerControls playerControls;
+    CharacterController controller;
 
-    // private void Awake() {
-        
-    // }
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
 
-    // private void Start() {
+        playerControls = new();
+    }
 
-    // }
+    private void OnEnable() {
+        playerControls.Movement.Enable();
+    }
 
-    public float speed = 3.0F;
-    public float rotateSpeed = 3.0F;
+    private void OnDisable() {
+        playerControls.Movement.Disable();
+    }
+
+    private void Start() {
+
+    }
 
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
+        Move();
+    }
 
-        // Rotate around y - axis
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+    private void Move() {
+        // if (playerControls.Movement.Forward.IsPressed()) {
+        //     Vector3 forward = transform.TransformDirection(Vector3.forward);
+        //     controller.SimpleMove(forward * forwardSpeed);
+        // }
 
-        // Move forward / backward
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        float curSpeed = speed * Input.GetAxis("Vertical");
-        controller.SimpleMove(forward * curSpeed);
+        // if (playerControls.Movement.Backward.IsPressed()) {
+        //     Vector3 back = transform.TransformDirection(Vector3.back);
+        //     controller.SimpleMove(back * backSpeed);
+        // }
+
+        // if (playerControls.Movement.StrafeLeft.IsPressed()) {
+        //     Vector3 left = transform.TransformDirection(Vector3.left);
+        //     controller.SimpleMove(left * strafeSpeed);
+        // }
+
+        // if (playerControls.Movement.StrafeRight.IsPressed()) {
+        //     Vector3 right = transform.TransformDirection(Vector3.right);
+        //     controller.SimpleMove(right * strafeSpeed);
+        // }
+
+        if (playerControls.Movement.Forward.IsPressed()) {
+            // Vector3 forward = transform.TransformDirection(Vector3.forward);
+            controller.SimpleMove(transform.forward * forwardSpeed);
+        }
+
+        if (playerControls.Movement.Backward.IsPressed()) {
+            // Vector3 back = transform.TransformDirection(Vector3.back);
+            controller.SimpleMove(-transform.forward * backSpeed);
+        }
+
+        if (playerControls.Movement.StrafeLeft.IsPressed()) {
+            // Vector3 left = transform.TransformDirection(Vector3.left);
+            controller.SimpleMove(-transform.right * strafeSpeed);
+        }
+
+        if (playerControls.Movement.StrafeRight.IsPressed()) {
+            // Vector3 right = transform.TransformDirection(Vector3.right);
+            controller.SimpleMove(transform.right * strafeSpeed);
+        }
     }
 }
