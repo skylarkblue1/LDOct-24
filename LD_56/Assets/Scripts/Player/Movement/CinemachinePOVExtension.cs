6ca257bb6551f02b54ad327b5cb1572865235e88
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-10-04 22:16:51 -0700

    Update CinemachinePOVExtension.cs
    Add null condition to remove error messages outside of playmode

--- LD_56/Assets/Scripts/Player/Movement/CinemachinePOVExtension.cs
+++ LD_56/Assets/Scripts/Player/Movement/CinemachinePOVExtension.cs
@@ -25,12 +25,12 @@ public class CinemachinePOVExtension : CinemachineExtension
         if (vcam.Follow) {
             if (stage == CinemachineCore.Stage.Aim) {
                 if (startingRotation == null) startingRotation = transform.localRotation.eulerAngles;
+                if (inputManager == null) return;
                 Vector2 deltaInput = inputManager.GetMouseDelta();
                 startingRotation.x += deltaInput.x * verticalSpeed * Time.deltaTime;
                 startingRotation.y += deltaInput.y * horizontalSpeed * Time.deltaTime;
                 startingRotation.y = Mathf.Clamp(startingRotation.y, -clampAngle, clampAngle);
                 state.RawOrientation = Quaternion.Euler(-startingRotation.y, startingRotation.x, 0f);
-
             }
         }
     }
