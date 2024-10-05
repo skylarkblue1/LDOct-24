dfa7bea8ad7e7964f47877d3640e5f8d72bfe672
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-10-04 17:21:00 -0700

    create player and set-up input actions

---
+++ LD_56/Assets/Scripts/Player/Movement/FirstPersonMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonMovement : MonoBehaviour
+{
+    // [SerializeField]
+    // float forwardSpeed;
+    // [SerializeField]
+    // float backSpeed;
+
+    // [SerializeField]
+    // float strafeSpeed;
+
+    // private void Awake() {
+        
+    // }
+
+    // private void Start() {
+
+    // }
+
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        // Move forward / backward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
